2494f1eb88bc1f57c281bc8fb410c26dabb53743
Ivan Serenko <ivan.serenko@greathead.net>
2020-08-21 22:56:07 +0300

    - add fragment mixin

--- src/Timeline/Fragment.jsx
+++ src/Timeline/Fragment.jsx
@@ -4,17 +4,18 @@ import styles from './TimeLine.module.sass'
 import clsx from 'clsx'
 
 export const Fragment = (props) => {
-  const { id, width, active, className, last, first, label, onClick } = props
+  const { id, width, active, className, last, first, onClick, alter } = props
 
   const classes = clsx({
     [styles.active]: active,
     [styles.first]: first,
     [styles.last]: last,
+    [styles.alter]: alter,
   }, styles.fragment, className)
 
   return (
     <div className={classes} style={{ minWidth: `${width}%` }} onClick={() => onClick(id)}>
-      {label}
+      {active ? 'MUDA' : 'ORA' }
     </div>
   )
 }
--- src/stories/TimeLine.stories.js
+++ src/stories/TimeLine.stories.js
@@ -7,23 +7,16 @@ export default {
 };
 
 const items = [
-  { id: 1, label: '123213', active: false },
-  { id: 2, label: '123213', active: true },
-  { id: 3, label: '123213', active: false, width: 5 },
-  { id: 4, label: '123213', active: true },
-  { id: 5, label: '123213', active: true },
-  { id: 6, label: '123213', active: false, width: 10 },
-  { id: 7, label: '123213', active: true, width: 15 },
-  { id: 8, label: '123213', active: false },
-  { id: 9, label: '123213', active: true },
-  { id: 10, label: '123213', active: false },
-  { id: 11, label: '123213', active: false },
-  { id: 12, label: '123213', active: false },
-  { id: 13, label: '123213', active: false },
-  { id: 14, label: '123213', active: false },
-  { id: 15, label: '123213', active: false },
-  { id: 16, label: '123213', active: false },
-  { id: 17, label: '123213', active: false },
+  { id: 1, active: false, width: 20 },
+  { id: 2, active: true },
+  { id: 3, active: false, width: 5 },
+  { id: 4, active: true, alter: true },
+  { id: 5, active: true },
+  { id: 6, active: false, width: 10 },
+  { id: 7, active: true, width: 15 },
+  { id: 8, active: false, alter: true },
+  { id: 9, active: true, alter: true },
+  { id: 10, active: false, alter: true },
 ]
 
 export const TimeLineStates = () => {
@@ -39,7 +32,7 @@ export const TimeLineStates = () => {
   }
 
   return (
-  <div style={{ backgroundColor: 'white', padding: '20px', height: '500px'}}>
+  <div style={{ backgroundColor: '#555', padding: '20px', height: '500px'}}>
     <TimeLine items={itemsState} onFragmentClick={handleClick} />
   </div>
 )};
